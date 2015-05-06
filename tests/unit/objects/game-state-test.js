d2e3d165fbd3ddecc6d36b15410ce9f6ed9d40fc
Tristan Siegel <tristan.t.siegel@gmail.com>
2015-05-05 21:32:30 -0400

    creates game state object

---
+++ tests/unit/objects/game-state-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('object:game-state', 'GameState', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  var object = this.subject();
+  // var store = this.store();
+  assert.ok(!!object);
+});
